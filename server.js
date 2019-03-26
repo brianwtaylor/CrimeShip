420543aeacf819a8cf509d3a2c5c82b9e0c709a8
briantaylor <brian.taylor818@gmail.com>
2019-03-26 11:32:36 -0700

    Connectes Mongo Atlas DB to server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// newUrlParser removes deprecation warning from mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('Mongoose Connected'))
+  .catch(err => console.log(err))
 
 const server = new ApolloServer({
   typeDefs,
