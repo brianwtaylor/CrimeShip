a2127d88ca9a48aaf48f50c9b6b80b979b8573a2
briantaylor <brian.taylor818@gmail.com>
2019-03-26 12:30:07 -0700

    Adds Google Login Button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token
+    console.log(idToken);
+  }
+
+  return <GoogleLogin 
+    clientId="827258769707-jlad19iut5g4rsk0pgk8t39l56jrs40n.apps.googleusercontent.com"
+    onSuccess={onSuccess}
+    isSignedIn={true}
+   />;
 };
 
 const styles = {
