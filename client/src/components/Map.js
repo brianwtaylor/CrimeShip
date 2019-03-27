af92b0986f546ef9235e31027efddeeee6a17c5a
briantaylor <brian.taylor818@gmail.com>
2019-03-26 17:06:25 -0700

    Adds MapBox API, creates and styles Map component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 34.0522,
+  longitude: -118.2437,
+  zoom: 13
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYnJpYW53dGF5bG9yIiwiYSI6ImNqdHFlbzhhZzBlbjA0NG9jNHh5MjltZGMifQ.i8-mAsuJhhcZp7pC_LCKCA"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+      {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />   
+        </div>
+      </ReactMapGL>
+    </div>
+  )
 };
 
 const styles = {
